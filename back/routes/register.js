0ead7fc9799d4ab1031ea30b97c07e86471d4b51
Adrien Mareel <mareel.adri@yahoo.com>
2023-02-12 11:28:36 +0100

    Register part (front and back) , still need to hash password

--- back/routes/register.js
+++ back/routes/register.js
@@ -1,12 +1,35 @@
 const jwt = require('jsonwebtoken');
+const mongoose = require('mongoose');
+const userSchema = require('../models/User');
 
 const checkUser = async (username, password) => {
     //check if user exists in db
+	const User = mongoose.model('users', userSchema);
+
+	User.findOne({ username: username }, (err, user) => {
+		if (err) {
+			console.log(err);
+			return true;
+		}
+		if (user) return true;
+		else return false;
+	});
+
     return false;
 }
 
 const createUser = async (username, password) => {
     //create user in db
+	const User = mongoose.model('users', userSchema);
+
+	User.create({ username: username, password: password }, (err, user) => {
+		if (err) {
+			console.log(err);
+			return { error : "Error creating User"};
+		}
+		return true;
+	});
+
 }
 
 module.exports = (app) => {
@@ -14,6 +37,7 @@ module.exports = (app) => {
     app.post('/register', async (req, res) => {
         const { username, password } = req.body;
 
+		//check if user exists
         const userExists = await checkUser(username, password);
 
         if (userExists) {
@@ -21,11 +45,12 @@ module.exports = (app) => {
         }
 
         //create user in db
-        createUser(username, password);
+        const user = await createUser(username, password);
+		if(user.error) return res.status(401).json({ error: user.error });
 
         const token = jwt.sign({username}, "my-secret", {expiresIn : '1h'});
         res.cookie('token', token, { httpOnly: true });
 
-        return res.redirect('/list');
+        return res.status(200).json({ message: 'User created' });
     });
 }
--- front/login.js
+++ front/login.js
@@ -1,26 +1,44 @@
 const form = document.querySelector('.form');
+let path = '/login';
+
+document.getElementById('register').addEventListener('click', () => {
+	path = '/register';
+	document.getElementById('register').style.display = "none";
+	document.getElementById('login').style.display = "block";
+	document.getElementById('title').innerHTML = "Register";
+	document.getElementsByClassName('submit-button')[0].innerHTML = "Register";
+});
+
+document.getElementById('login').addEventListener('click', () => {
+	path = '/login';
+	document.getElementById('login').style.display = "none";
+	document.getElementById('register').style.display = "block";
+	document.getElementById('title').innerHTML = "Login";
+	document.getElementsByClassName('submit-button')[0].innerHTML = "Login";
+});
+
 
 form.addEventListener('submit', (e) => {
-    e.preventDefault();
+	e.preventDefault();
 
-    fetch('/login', {
-        method: 'POST',
-        headers: {
-            'Content-Type': 'application/json'
-        },
-        body: JSON.stringify({
-            username: form.username.value,
-            password: form.password.value
-        })
-    })
-        .then(res => res.json())
-        .then(data => {
-            if (data.error) {
-                document.getElementById('error').innerHTML = data.error;
-                document.getElementById('error').style.display = "flex";
-                document.getElementById('error').style.opacity = "1";
-            } else {
-                window.location.href = '/list';
-            }
-        })
+	fetch(path, {
+		method: 'POST',
+		headers: {
+			'Content-Type': 'application/json'
+		},
+		body: JSON.stringify({
+			username: form.username.value,
+			password: form.password.value
+		})
+	})
+		.then(res => res.json())
+		.then(data => {
+			if (data.error) {
+				document.getElementById('error').innerHTML = data.error;
+				document.getElementById('error').style.display = "flex";
+				document.getElementById('error').style.opacity = "1";
+			} else {
+				window.location.href = '/list';
+			}
+		})
 });
