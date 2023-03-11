43c0d401cfa9088b4e106227eeab32fc1a7fcc60
Adrien Mareel <56940107+AdriMareel@users.noreply.github.com>
2023-03-11 22:25:41 +0100

    Delete list.js

--- back/routes/list.js
+++
@@ -1,8 +0,0 @@
-const {cookieAuthJWT} = require('../middleware/cookieAuthJWT');
-
-module.exports = (app) => {
-  	app.post("/list", cookieAuthJWT, (req, res, ) => {
-    	console.log(req.user);
-    	res.redirect("/list");
-  	});
-}
